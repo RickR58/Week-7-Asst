9ee697b717e512572e5e22d825fb3fc176f079b4
RickR58 <rick.robb58@gmail.com>
2024-11-26 15:31:05 -0700

    Finish up to turn in for asst.

--- WeekSevenAsst.js
+++ WeekSevenAsst.js
@@ -96,10 +96,78 @@ function isSumGreaterThan100(numbers) {
 console.log(isSumGreaterThan100([10, 50])); // false
 console.log(isSumGreaterThan100([50, 60, 5])); // true
 
-//Write a function that takes an array of numbers and returns the average of all the elements in the array.
+//10 Write a function that takes an array of numbers and returns the average of all the elements in the array.
+// NEED: 1)something that will total the numbers. 2) Something that will count the qty of numbers. 3) Something that will divide #1 by #2 to average those numbers
+function getAvg(numbers) {
+  if (numbers.length === 0) {
+    return 0; // So as to not divide by 0
+  }
+  const newSum = numbers.reduce((accumulator, currentValue) => {
+    //.reduce callback takes the values defiend by the getAvg function and reduces them to a single number.
+    return accumulator + currentValue;
+  }, 0); // accumulator is returning the total value from the previous iteration.
+  const average = newSum / numbers.length; // this function (average) takes the result of the previous const and divides it by the total elements in  the array.
+  return average;
+}
+const nums = [10, 5, 20, 15, 30, 25, 40, 45];
+const avg = getAvg(nums);
+console.log(avg); //badda bing, badda boom, and bob's yer uncle. I can change nums and get a different average.
 
-//Write a function that takes two arrays of numbers and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
+//11. Write a function that takes two arrays of numbers and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
 
-//Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
+function figureAverage(numbers) {
+  if (numbers.length === 0) {
+    return 0; //returns 0 if array empty
+  }
 
-//Create a function of your own that solves a problem. In comments, write what the function does and why you created it.
+  const sum3 = numbers.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+  return sum3 / numbers.length; // calculates the average and returns it
+}
+
+function isAverageGreater(array1, array2) {
+  const average1 = figureAverage(array1); //This line and next calculate the averages for the individ. arrays.
+  const average2 = figureAverage(array2);
+  return average1 > average2; //compares the avg
+}
+const array1 = [10, 20, 30, 40];
+const array2 = [5, 10, 15, 20];
+const result2 = isAverageGreater(array1, array2); //checks if first average is greater than second and...
+
+console.log(result2); // in this case, prints "true" but tested by adding more numbers into array2 which returns "false."
+
+//12. Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
+
+//function willBuyDrink (willBuyDrink, moneyInPocket);
+// isHotOutside which should be a true/false response AND....
+// is moneyInPocket > 10.5
+// if last is true, return "It's hot outside, buy yourself a drink"
+// otherwise return "You don't have enough money. Find some shade."
+/* write a function  called willBuyDrink with two parameters (isHotOutside, moneyInPocket).
+first check if temperature is above 90°. If true, check if moneyInPocket is >= 10.50. If that is true, print "It's hot outside and you have enough money, buy yourself a drink." 
+if both parameters are false, print "You don't have enough money. Find some shade."*/
+
+//NOTE: While the following solution seems to work, I feel like it should have been more like the above comments in which it is first determined if it is hot (what temperature) and then, if that is above, say, 90, will look at how much money I have and, if that's above 10.50 will return a statement saying I can buy a drink and, if not, a statement saying I can't afford a drink. That said, I couldn't seem to create a solution.
+
+function willBuyDrink(isHotOutside, moneyInPocket) {
+  return isHotOutside && moneyInPocket > 10.5;
+}
+console.log(willBuyDrink(true, 10.49));
+
+//13. Create a function of your own that solves a problem. In comments, write what the function does and why you created it.
+
+//if 1.3 lbs of turkey will feed 1 person, how many friends can I feed with one X-lb turkey?
+
+function calcFriendsFed(turkeySize) {
+  const friendsFed = turkeySize / 1.3; //calculate how many friends can be fed with a given turkey size if 1.3 lbs feeds one person
+  return Math.floor(friendsFed); //Rounds the number of people down to get awhole number
+}
+const turkeySize = 24.7; // Can change the size to get a correct number
+
+const friendsFed = calcFriendsFed(turkeySize);
+console.log(
+  `You can feed about ${friendsFed} people with a ${turkeySize} pound turkey.`
+);
+//I had issues with getting the strings to display right because I wasn't using backticks at first.
